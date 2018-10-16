e981875f5ecebd6a198c1b5d7f4b63e73feb4610
IgorK <likep@mail.ru>
2018-10-16 20:22:55 +0700

    Добавит обработчик клика на кнопку

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (ev) {
+    document.querySelector("button").addEventListener("click", function (ev) {
+        alert ("Кнопка Активирована!");
+    });
+});
